1bd104c3764c2131ec9d3438c1c67e45416256b4
Irene <irina_gorlach@mail.ru>
2020-11-06 21:55:56 +0300

    adding travel folder

---
+++ travel/travel.js
@@ -0,0 +1,21 @@
+let points = document.querySelectorAll(".points");
+let singlePoint = document.createElement("span");
+singlePoint.innerHTML = ".";
+
+function pointActive(point) {
+  point.classList.remove("points");
+  point.classList.add("active");
+}
+
+function pointInactive(point) {
+  point.classList.remove("active");
+  point.classList.add("points");
+}
+
+points.forEach((point) =>
+  point.addEventListener("mouseover", () => pointActive(point))
+);
+
+points.forEach((point) =>
+  point.addEventListener("mouseout", () => pointInactive(point))
+);
